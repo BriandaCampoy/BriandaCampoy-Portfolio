10ecacc7a91cf2fcaf9115d385aba19cde520d04
Brianda Campoy <brianda.campoy@gmail.com>
2023-07-01 15:17:50 -0700

    Change to the mainData text

--- src/Components/MainData.jsx
+++ src/Components/MainData.jsx
@@ -8,11 +8,11 @@ const MainData = () => {
     <section className="main">
     <div className="main__content">
         <p className="main__text">
-            I am a Software Engineer Student, since 2019. I have worked in several projects in and out of school, and I am ready to work in more.<br/>
-            At the begenning of my education I was part of a software development workshop and where I learned basic skills on database and development of software desktop applications. 
-            Then I started to learn web development and now I can work in both.
+        I am software engineer with a focus on web development. With experience in several projects as technological solutions for different businesses and organizations. 
+        I can work with a full stack, but my specialty is front-end with React. 
+        Developing and getting involved in projects is one of my favorite things, and I am always looking to learn new things and contribute to great projects.
             <br/>
-            My interest in technology started a long time ago when I was a child and I knew the videogames, I spent my younghood playing videogames in a Nintendo 3DS that is because I based my portfolio in the interfaces of one of this consoles.
+            My interest in technology started a long time ago when I was a child and I knew about video games. I spent my youth playing video games on a Nintendo 3DS, which is why I based my portfolio on the interfaces of one of these consoles.
         </p>
         <p className="main__text">
             I can work with:
